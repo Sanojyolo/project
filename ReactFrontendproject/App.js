9430ae49d4113db38e022ada8b427143b5268177
Sanoj S <88619381+Sanojyolo@users.noreply.github.com>
2022-03-08 17:42:31 +0530

    Add files via upload

---
+++ ReactFrontendproject/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Weather from "./Components/Weather"
+
+
+
+
+
+ 
+   const App =() => {
+      return(
+          <Weather/>
+        
+      )
+  }
+
+
+//jsx
+// 
+export default App;
